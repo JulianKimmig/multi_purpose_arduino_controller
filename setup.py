0d85d606793a7afdc17ae990e7776e6515265267
julian Stobbe <julian-stobbe@gmx.de>
2019-06-13 07:49:32 +0200

    rename

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup = dict(
     long_description_content_type="text/markdown",
     #py_modules=["multi_purpose_arduino_controller"],
     url="https://github.com/JulianKimmig/multi_purpose_arduino_controller",
+    include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=["numpy","pandas","pyserial","django-bootstrap4","custom-accounts-django","django","django-glrm"],
     classifiers=[
